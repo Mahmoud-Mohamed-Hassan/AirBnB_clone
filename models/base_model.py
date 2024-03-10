df86221cd4378f74d972150c70afb3bbad37004c
Mahmoud Mohamed <mahmoudmohamedhassann@gmail.com>
2024-03-10 15:59:50 -0700

    a

--- models/base_model.py
+++ models/base_model.py
@@ -6,25 +6,22 @@ import uuid
 from datetime import datetime
 import models
 
-
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        ISO_time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        time_form = "%Y-%m-%dT%H:%M:%S.%f"
 
         if kwargs:  # takes kwargs from dict
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, ISO_time_format))
+                    setattr(self, key, datetime.strptime(value, time_form))
                 else:  # kwargs is id
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-
         models.storage.new(self)
 
     def save(self):
